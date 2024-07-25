625d352cd9064aacaef018be8312735755b802c3
Flower CA77 <ca538468142@outlook.com>
2024-07-25 11:47:36 +0800

    clean master branch to nothing(because nothing is finished)

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,17 @@
+import InfNumber;
+
 import <iostream>;
 import <limits>;
 
 int main()
 {
+	InfInt n(-240);
+	auto m = n.Abs();
+	std::cout << std::format("{}, {}", n, *m) << std::endl;
+	n.BaseConvert(16);
+	std::cout << n << std::endl;
+	InfInt var("28515155", 10, -1);
+	std::cout << std::format("{}, {}, {}", var, *var.Abs(), var.ToInt()) << std::endl;
+	InfInt var_24(-451645142, 24);
+	std::cout << var_24 << std::endl;
 }
